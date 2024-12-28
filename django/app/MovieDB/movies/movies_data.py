838d765be8b59af5e7370e903ce7c170143e1b5c
rzdabbah@gmail.com <rzdabbah@gmail.com>
2024-12-28 14:26:05 +0200

    singlton

--- django/app/MovieDB/movies/movies_data.py
+++ django/app/MovieDB/movies/movies_data.py
@@ -4,16 +4,20 @@ import os
 from bson.json_util import dumps
 
 
-class MoviesMngr:
+class MoviesMngr():
+
+    def __init__(self):
+        self.client = None
+
     def getConnection(self):
+        if self.client != None:
+            return self.client
         mongo_host = os.environ.get("MONGO_DB_HOST", "localhost")
         mongo_port = os.environ.get("MONGO_DB_PORT", "27017")
-        mongo_db_user = os.environ.get("MONGO_DB_USERNAME")
-        mongo_db_pw = os.environ.get("MONGO_DB_PASSWORD")
-        mongo_db_name = os.environ.get("MONGO_DB_NAME")
+        mongo_db_user = os.environ.get("MONGO_DB_USERNAME","")
+        mongo_db_pw = os.environ.get("MONGO_DB_PASSWORD","")
         db_url = f"mongodb://{mongo_db_user}:{mongo_db_pw}@{mongo_host}:{mongo_port}/"
-        db_url = 'mongodb://localhost:27017/'
-        # MongoDB connection
+        # MongoDB connection  should be singletone 
         self.client = MongoClient(db_url)
         return self.client
     
